273246af5f8065f6ddf254985d7f3548469f13f4
pjh <pjh402941@gmail.com>
2023-08-15 13:21:37 +0900

    함수형으로 수정

--- mh_tobuy/src/pages/pay/productDetail.jsx
+++ mh_tobuy/src/pages/pay/productDetail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
@@ -199,206 +199,189 @@ const My = styled.div`
   cursor: pointer;
 `;
 
-class ProductDetail extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: 1,
-      price: 15000,
-    };
-  }
+const ProductDetail = () => {
+  const [value, setValue] = useState(1);
+  const price = 15000;
 
-  inputValue = (e) => {
+  const inputValue = (e) => {
     const { value } = e.target;
-    this.setState({
-      value: value,
-    });
+    setValue(value);
   };
 
-  addValue = () => {
-    const { value } = this.state;
+  const addValue = () => {
     if (value > 19) {
       return;
     }
-    this.setState({
-      value: value + 1,
-    });
+    setValue(value + 1);
   };
 
-  minusValue = () => {
-    const { value } = this.state;
+  const minusValue = () => {
     if (value < 2) {
       return;
     }
-    this.setState({
-      value: value - 1,
-    });
+    setValue(value - 1);
   };
-  navigateToBack = () => {
+
+  const navigate = useNavigate();
+
+  const navigateToBack = () => {
     window.history.back();
   };
-  navigateToVideo = () => {
-    //이거 오류남.. 왜지
-    const { navigate } = this.props; // useNavigate 훅으로부터 가져온 navigate 함수
+
+  const navigateToVideo = () => {
     navigate("/PlayVideo");
   };
 
-  render() {
-    const { value, price } = this.state;
-    const totalPriceValue = value * price;
+  const totalPriceValue = value * price;
 
-    const submitStyle = {
-      width: "222px",
-      height: "53px",
-      padding: "10px",
-      borderRadius: "6px",
-      background: "#05BBA2",
-      boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+  const submitStyle = {
+    width: "222px",
+    height: "53px",
+    padding: "10px",
+    borderRadius: "6px",
+    background: "#05BBA2",
+    boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+    border: "none",
+    color: "white",
+  };
+
+  const countStyles = {
+    minus: {
+      borderRadius: "50%",
+      width: "24px",
+      height: "24px",
+      border: "1px solid #60716F",
+      background: "transparent",
+      cursor: "pointer", // Adding cursor pointer for better interaction
+    },
+    plus: {
+      borderRadius: "50%",
+      width: "24px",
+      height: "24px",
+      border: "1px solid #60716F",
+      background: "transparent",
+      cursor: "pointer", // Adding cursor pointer for better interaction
+    },
+    inputCount: {
+      width: "26px", // Adjusted width for better alignment
       border: "none",
-      color: "white",
-    };
-    const countStyles = {
-      minus: {
-        borderRadius: "50%",
-        width: "24px",
-        height: "24px",
-        border: "1px solid #60716F",
-        background: "transparent",
-        cursor: "pointer", // Adding cursor pointer for better interaction
-      },
-      plus: {
-        borderRadius: "50%",
-        width: "24px",
-        height: "24px",
-        border: "1px solid #60716F",
-        background: "transparent",
-        cursor: "pointer", // Adding cursor pointer for better interaction
-      },
-      inputCount: {
-        width: "26px", // Adjusted width for better alignment
-        border: "none",
-        textAlign: "center",
-        margin: "0 10px", // Adjusted margin for better alignment
-      },
-    };
+      textAlign: "center",
+      margin: "0 10px", // Adjusted margin for better alignment
+    },
+  };
 
-    return (
-      <Container>
-        <BodyWrapper>
-          <Topbar>
-            <Back onClick={this.navigateToBack}>
-              <img
-                src={`${process.env.PUBLIC_URL}/images/left.png`}
-                alt="back"
-                width="12px"
-              />
-            </Back>
-            <Logo>
-              <img
-                src={`${process.env.PUBLIC_URL}/images/로고3.png`}
-                alt="logo"
-                width="90px"
-              />
-            </Logo>
-            <Video onClick={this.navigateToVideo}>
-              <img
-                src={`${process.env.PUBLIC_URL}/images/carousel-video.png`}
-                width="30px"
-                alt="video"
-              />
-            </Video>
-          </Topbar>
+  return (
+    <Container>
+      <BodyWrapper>
+        <Topbar>
+          <Back onClick={navigateToBack}>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/left.png`}
+              alt="back"
+              width="12px"
+            />
+          </Back>
+          <Logo>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/로고3.png`}
+              alt="logo"
+              width="90px"
+            />
+          </Logo>
+          <Video onClick={navigateToVideo}>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/carousel-video.png`}
+              width="30px"
+              alt="video"
+            />
+          </Video>
+        </Topbar>
 
-          <Body>
-            <FormContent>
-              <PayImg>
-                <img
-                  src={`${process.env.PUBLIC_URL}/images/productSample.png`}
-                  width="326px"
-                  height="326px"
-                ></img>
-              </PayImg>
-              <ProductName>상품명</ProductName>
-              <Gra></Gra>
-              <ProductWrapper>
-                <PriceWrapper>
-                  <Price>{price.toLocaleString()}</Price>
-                  <Won> 원</Won>
-                </PriceWrapper>
-                <CountWrapper>
-                  <button
-                    className="minus"
-                    style={countStyles.minus}
-                    onClick={this.minusValue}
-                  >
-                    -
-                  </button>
-                  <input
-                    className="inputCount"
-                    style={countStyles.inputCount}
-                    onChange={this.inputValue}
-                    value={value}
-                  ></input>
-                  <button
-                    className="plus"
-                    style={countStyles.plus}
-                    onClick={this.addValue}
-                  >
-                    +
-                  </button>
-                </CountWrapper>
-              </ProductWrapper>
-              <Gra></Gra>
-              <TotalWrapper>
-                <Whole>총 </Whole>
-                <PriceWrapper>
-                  <TotalPrice>{totalPriceValue.toLocaleString()}</TotalPrice>
-                  <Won> 원</Won>
-                </PriceWrapper>
-              </TotalWrapper>
-            </FormContent>
-            <Submit>
-              <button formAction="" style={submitStyle}>
-                구매하기
-              </button>
-            </Submit>
-            <CoachMark>
+        <Body>
+          <FormContent>
+            <PayImg>
               <img
-                src={`${process.env.PUBLIC_URL}/images/coachmark.png`}
-                width="48px"
-              />
-            </CoachMark>
-          </Body>
-          <BottomBar>
-            <Menu>
-              <img
-                src={`${process.env.PUBLIC_URL}/images/menu.png`}
-                width="26px"
-              />
-            </Menu>
-            <Search>
-              <img
-                src={`${process.env.PUBLIC_URL}/images/search.png`}
-                width="26px"
-              />
-            </Search>
-            <Home>
-              <img
-                src={`${process.env.PUBLIC_URL}/images/home.png`}
-                width="26px"
-              />
-            </Home>
-            <My>
-              <img
-                src={`${process.env.PUBLIC_URL}/images/me.png`}
-                width="26px"
-              />
-            </My>
-          </BottomBar>
-        </BodyWrapper>
-      </Container>
-    );
-  }
-}
-
+                src={`${process.env.PUBLIC_URL}/images/productSample.png`}
+                width="326px"
+                height="326px"
+              ></img>
+            </PayImg>
+            <ProductName>상품명</ProductName>
+            <Gra></Gra>
+            <ProductWrapper>
+              <PriceWrapper>
+                <Price>{price.toLocaleString()}</Price>
+                <Won> 원</Won>
+              </PriceWrapper>
+              <CountWrapper>
+                <button
+                  className="minus"
+                  style={countStyles.minus}
+                  onClick={minusValue}
+                >
+                  -
+                </button>
+                <input
+                  className="inputCount"
+                  style={countStyles.inputCount}
+                  onChange={inputValue}
+                  value={value}
+                ></input>
+                <button
+                  className="plus"
+                  style={countStyles.plus}
+                  onClick={addValue}
+                >
+                  +
+                </button>
+              </CountWrapper>
+            </ProductWrapper>
+            <Gra></Gra>
+            <TotalWrapper>
+              <Whole>총 </Whole>
+              <PriceWrapper>
+                <TotalPrice>{totalPriceValue.toLocaleString()}</TotalPrice>
+                <Won> 원</Won>
+              </PriceWrapper>
+            </TotalWrapper>
+          </FormContent>
+          <Submit>
+            <button formAction="" style={submitStyle}>
+              구매하기
+            </button>
+          </Submit>
+          <CoachMark>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/coachmark.png`}
+              width="48px"
+            />
+          </CoachMark>
+        </Body>
+        <BottomBar>
+          <Menu>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/menu.png`}
+              width="26px"
+            />
+          </Menu>
+          <Search>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/search.png`}
+              width="26px"
+            />
+          </Search>
+          <Home>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/home.png`}
+              width="26px"
+            />
+          </Home>
+          <My>
+            <img src={`${process.env.PUBLIC_URL}/images/me.png`} width="26px" />
+          </My>
+        </BottomBar>
+      </BodyWrapper>
+    </Container>
+  );
+};
 export default ProductDetail;
